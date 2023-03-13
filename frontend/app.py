c9589605674d9206e211ebf140838af33552b20b
luiz <luiz_tauffer@hotmail.com>
2023-03-13 10:59:55 +0100

    updates

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,20 @@
-from dash import Dash, dcc, html, Input, Output, State, ctx, no_update
+from dash import Dash, dcc, html, Input, Output, State, ctx, no_update, DiskcacheManager, CeleryManager
 import dash_bootstrap_components as dbc
-from utils import parse_pdf
+from copy import deepcopy
+from utils import pre_parse_pdf, add_pdf_to_db
+
+
+# if 'REDIS_URL' in os.environ:
+# Use Redis & Celery if REDIS_URL set as an env variable
+# from celery import Celery
+# celery_app = Celery(__name__, broker=os.environ['REDIS_URL'], backend=os.environ['REDIS_URL'])
+# background_callback_manager = CeleryManager(celery_app)
+
+# else:
+# Diskcache for non-production apps when developing locally
+import diskcache
+cache = diskcache.Cache("./cache")
+background_callback_manager = DiskcacheManager(cache)
 
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.LITERA])
@@ -13,36 +27,31 @@ title_image = html.Img(
     style={'width': '300px', 'display': 'block', 'margin-left': 'auto', 'margin-right': 'auto'}
 )
 
+navigate_buttons_style = {
+    'width': '200px', 
+    'display': 'block', 
+    'margin-left': 'auto', 
+    'margin-right': 'auto',
+    'margin-top': '8px',
+    'color': GRAY, 
+    'background-color': 'white',
+    'border-color': 'white', 
+    'border-width': '0px',
+    'font-size': '1.3rem', 
+    'line-height': '1.5', 
+    'font-weight': '700',
+}
+
 navigate_question = html.Button(
     'Question',
     id='navigate-question',
-    style={
-        'width': '200px', 
-        'display': 'block', 
-        'margin-left': 'auto', 
-        'margin-right': 'auto',
-        'margin-top': '8px',
-        'color': GRAY, 
-        'background-color': 'white',
-        'border-color': 'white', 'border-width': '0px',
-        'font-size': '1.3rem', 'line-height': '1.5', 'font-weight': '700',
-    }
+    style=navigate_buttons_style
 )
 
 navigate_add = html.Button(
     'Add Document',
     id='navigate-add',
-    style={
-        'width': '200px', 
-        'display': 'block', 
-        'margin-left': 'auto', 
-        'margin-right': 'auto',
-        'margin-top': '8px',
-        'color': GRAY, 
-        'background-color': 'white',
-        'border-color': 'white', 'border-width': '0px',
-        'font-size': '1.3rem', 'line-height': '1.5', 'font-weight': '700',
-    }
+    style=navigate_buttons_style
 )
 
 first_row = dbc.Row(
@@ -87,11 +96,87 @@ question_button = dbc.Button(
     }
 )
 
+answer_text = html.Div(
+    children=[],
+    id='answer-text',
+    style={
+        'width': '600px', 
+        'height': '200px',
+        'display': 'block', 
+        'margin-left': 'auto', 
+        'margin-right': 'auto',
+        'margin-top': '8px',
+        'color': GRAY, 
+        'background-color': 'white',
+        'border-color': GRAY, 
+        'border-radius': '0.25rem', 
+        'border-width': '1px',
+        'border-style': 'solid',
+        'font-size': '1.1rem',
+        'box-shadow': '0px 3px 4px rgb(0 0 0 / 70%)',
+    }
+)
+
+references_button = dbc.Button(
+    "References", 
+    id="button-references", 
+    color="primary", 
+    style={
+        'width': '120px', 
+        'display': 'block', 
+        'margin-left': 'auto', 
+        'margin-right': 'auto',
+        'margin-top': '0px',
+        'color': GRAY, 
+        'background-color': 'white',
+        'border-color': GRAY, 'border-radius': '0.25rem', 'border-width': '1px',
+        'font-size': '0.9rem', 
+        'font-weight': '400',
+    }
+)
+
+collapsible_references = dbc.Collapse(
+    dbc.Row(
+        children=[
+            dbc.Col(
+                dbc.Card(
+                    [
+                        dbc.CardHeader("Title, Author, Year"),
+                        dbc.CardBody([html.P("This is some card text", className="card-text")]),
+                    ]
+                )
+            ),
+            dbc.Col(
+                dbc.Card(
+                    [
+                        dbc.CardHeader("Title, Author, Year"),
+                        dbc.CardBody([html.P("This is some card text", className="card-text")]),
+                    ]
+                )
+            ),
+            dbc.Col(
+                dbc.Card(
+                    [
+                        dbc.CardHeader("Title, Author, Year"),
+                        dbc.CardBody([html.P("This is some card text", className="card-text")]),
+                    ]
+                )
+            ),
+        ],
+        style={"margin-left": "5rem", "margin-right": "5rem", "margin-top": "8px"},
+    ),
+    id="collapsible-references",
+    is_open=False,
+)
+
 question_component = html.Div(
     children=[
         text_input,
         question_button,
-        html.Div(id="output"),
+        html.Br(),
+        answer_text,
+        references_button,
+        collapsible_references,
     ],
     id='div-question-component',
 )
@@ -178,7 +263,7 @@ add_form = dbc.Form(
 
 add_document_button = dbc.Button(
     "Add document", 
-    id="button-question", 
+    id="button-add-document", 
     color="primary", 
     style={
         'width': '170px', 
@@ -193,45 +278,91 @@ add_document_button = dbc.Button(
     }
 )
 
+waiting_area_1 = dbc.Row(
+    children=[
+        # dbc.Col(
+        #     html.P("Processing and adding document...", id="txt-waiting", style={"visibility": "hidden"}),
+        #     width={"size": 3, "offset": 4},
+        #     style={"text-align": "center", "size": "100px"}
+        # ),
+        dbc.Col(
+            html.Img(id="img-waiting", style={"visibility": "hidden", "width": "70px", "height": "70px"}),
+            width={"size": 1, "offset": 6},
+        ),
+    ]
+)
+
 add_component = html.Div(
     children=[
         upload_area,
         html.Br(),
         add_form,
         add_document_button,
+        html.Br(),
+        waiting_area_1,
     ],
     id='div-add-component',
 )
 
 # App layout
+add_document_toast = dbc.Toast(
+    "",
+    id="add-document-toast",
+    is_open=False,
+    dismissable=True,
+    duration=10000,
+    icon="primary",
+    style={"position": "fixed", "top": 66, "right": 10, "width": 350},
+)
+
 app.layout = html.Div(
     children=[
         title_image,
+        add_document_toast,
         first_row,
         html.Br(),
         html.Br(),
         html.Div(
             id='div-page-content',
-            children=[add_component],
+            children=[question_component],
         )
     ]
 )
 
 
+# Callbacks Question
+@app.callback(
+    Output("collapsible-references", "is_open"),
+    [Input("button-references", "n_clicks")],
+    [State("collapsible-references", "is_open")],
+)
+def toggle_collapse_references(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+
+# Callbacks Add document
 @app.callback(
     Output('div-page-content', 'children'),
+    Output('navigate-question', 'style'),
+    Output('navigate-add', 'style'),
     Input('navigate-question', 'n_clicks'),
     Input('navigate-add', 'n_clicks'),
     State('div-page-content', 'children'),
 )
 def display_page(n_clicks_question, n_clicks_add, page_content):
     button_id = ctx.triggered_id
+    selected_style = deepcopy(navigate_buttons_style)
+    selected_style['border-bottom'] = f'2px solid {GRAY}'
+    unselected_style = deepcopy(navigate_buttons_style)
+    unselected_style['border-bottom'] = f'0px white'
     if button_id is None:
-        return no_update
+        return no_update, no_update, no_update
     elif button_id == 'navigate-question':
-        return question_component
+        return question_component, selected_style, unselected_style
     elif button_id == 'navigate-add':
-        return add_component
+        return add_component, unselected_style, selected_style
 
 
 @app.callback(
@@ -242,11 +373,47 @@ def display_page(n_clicks_question, n_clicks_add, page_content):
     State('upload-data', 'filename'),
     State('upload-data', 'last_modified')
 )
-def update_output(file_contents, file_name, last_modified):
+def update_forms(file_contents, file_name, last_modified):
     if file_contents is not None:    
-        metadata = parse_pdf(base64_pdf_bytestring=file_contents)
+        metadata = pre_parse_pdf(base64_pdf_bytestring=file_contents, use_openai=False)
         return metadata['title'], metadata['author'], metadata['year']
     return "", "", ""
+
+
+@app.callback(
+    Output('add-document-toast', 'children'),
+    Output('add-document-toast', 'is_open'),
+    Output('add-document-toast', 'icon'),
+    Input('button-add-document', 'n_clicks'),
+    State('upload-data', 'contents'),
+    State('input-title-row', 'value'),
+    State('input-author-row', 'value'),
+    State('input-year-row', 'value'),
+    background=True,
+    manager=background_callback_manager,
+    running=[
+        (Output("button-add-document", "disabled"), True, False),
+        (Output("img-waiting", "style"), {"visibility": "visible", "width": "70px", "height": "70px"}, {"visibility": "hidden"}),
+        (Output("img-waiting", "src"), "assets/images/book.gif", None)
+    ],
+    prevent_initial_call=True,
+)
+def add_document(n_clicks, file_contents, title, author, year):
+    if n_clicks is not None:
+        if file_contents is not None and title is not None and author is not None and year is not None:
+            result = add_pdf_to_db(
+                base64_pdf_bytestring=file_contents,
+                title=title,
+                author=author,
+                year=year
+            )
+            if result == "success":
+                return "Document added successfully!", True, "primary"
+            else:
+                return "Error adding document!", True, "danger"
+        else:
+            return "Missing document or information!", True, "danger"
+    return no_update, no_update, no_update
 
 
 if __name__ == '__main__':
--- frontend/utils.py
+++ frontend/utils.py
@@ -5,7 +5,7 @@ import os
 import openai
 
 
-def parse_pdf(base64_pdf_bytestring: str) -> dict:
+def pre_parse_pdf(base64_pdf_bytestring: str, use_openai: bool = False) -> dict:
     # Extract the base64 encoded data from the string
     base64_data = base64_pdf_bytestring.split(",")[1]
 
@@ -15,17 +15,16 @@ def parse_pdf(base64_pdf_bytestring: str) -> dict:
     # Create a PdfReader object using the byte stream
     reader = PdfReader(io.BytesIO(pdf_bytes))
 
-    first_page = reader.pages[0]
-
     pages_list = [p for p in reader.pages]
-    first_pages_content = " ".join([p.extract_text().replace("\n", " ").replace("..", "") for p in pages_list[0:4]])[0:3000]
-    return extract_metadata_openai(text_content=first_pages_content)
+    first_pages_content = " ".join([p.extract_text().replace("\n", " ").replace("..", "") for p in pages_list[0:5]])[0:2000]
+    if use_openai:
+        return extract_metadata_openai(text_content=first_pages_content)
+    else:
+        return {"title": "something", "author": "someone", "year": "2000"}
 
 
 def extract_metadata_openai(text_content: str) -> dict:
-
     openai.api_key = os.environ["OPENAI_API_KEY"]
-
     prompt = f"""Given the text below, extract these three pieces of information: 
 1) title: 
 2) author: 
@@ -54,3 +53,46 @@ Information:"""
         "year": response_string.split("year:")[1].split("\n")[0].strip(),
     }
     return metadata
+
+
+def add_pdf_to_db(base64_pdf_bytestring: str, title: str, author: str, year: str) -> None:
+    full_text = parse_full_pdf(base64_pdf_bytestring=base64_pdf_bytestring)
+    sentences = sentences_from_full_text(full_text=full_text, max_length=2000)
+    result = add_sentences_to_db(sentences=sentences, title=title, author=author, year=year)
+    return result
+
+
+def parse_full_pdf(base64_pdf_bytestring: str) -> dict:
+    # Extract the base64 encoded data from the string
+    base64_data = base64_pdf_bytestring.split(",")[1]
+
+    # Decode the base64 data to a byte stream
+    pdf_bytes = base64.b64decode(base64_data)
+
+    # Create a PdfReader object using the byte stream
+    reader = PdfReader(io.BytesIO(pdf_bytes))
+
+    pages_list = [p for p in reader.pages]
+    full_text = " ".join([p.extract_text().replace("\n", " ").replace("..", "") for p in pages_list])
+    return full_text
+
+
+def sentences_from_full_text(full_text: str, max_length: int=2000) -> list:
+    sentences = full_text.split(". ")
+    sentences_list = list()
+    last_sentence = sentences[0]
+    for i, s in enumerate(sentences[1:]):
+        combined_sentence = last_sentence + ". " + s
+        if len(combined_sentence) > 2000:
+            sentences_list.append(last_sentence)
+            last_sentence = s
+        else:
+            last_sentence = combined_sentence
+            if i == len(sentences) - 2:
+                sentences_list.append(last_sentence)
+    return sentences_list
+
+
+def add_sentences_to_db(sentences: list, title: str, author: str, year: str) -> None:
+    # TODO: vectorize and add sentences to db
+    return "success"
